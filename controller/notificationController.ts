70b308ab081b20a74b6bb7296c65c9c81b612664
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2023-11-02 18:20:45 +0200

    delete notifications

--- controller/notificationController.ts
+++ controller/notificationController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { CatchAsyncErrors } from "../middleware/catchAsyncErrors";
 import notificationModel from "../module/Notification";
 import ErrorHandler from "../utils/ErrorHandler";
+import cron from "node-cron";
 
 // get all notifications
 export const getAllNotifications = CatchAsyncErrors(
@@ -49,3 +50,14 @@ export const updateNotifications = CatchAsyncErrors(
     }
   }
 );
+
+// delete notifications
+
+cron.schedule("0 0 0 * * *", async () => {
+  const thiryDaysAge = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await notificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thiryDaysAge },
+  });
+  console.log("Delete read notifications");
+});
